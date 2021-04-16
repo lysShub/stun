57d1b4cd1ab5d50dd878321d44ba062d00743779
lysShub <2454148255@qq.com>
2021-04-16 10:48:08 +0800

    lysShb

--- judge.go
+++ judge.go
@@ -32,22 +32,6 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 		}
 	}
 
-	// 回复函数
-	var S = func(conn *net.UDPConn, da []byte, raddr *net.UDPAddr) error {
-		for i := 0; i < s.Iterate; i++ {
-			if raddr != nil {
-				if _, err := conn.WriteToUDP(da, raddr); err != nil {
-					return err
-				}
-			} else {
-				if _, err := conn.Write(da); err != nil {
-					return err
-				}
-			}
-		}
-		return nil
-	}
-
 	/* 开始 */
 	if step == 10 {
 		if len(da) != 20 {
@@ -62,7 +46,7 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 		D["c1"] = strconv.Itoa(int(da[18])<<8 + int(da[19])) // 第一使用端口
 		s.dbd.Ut(string(juuid), D)
 
-		if err = S(conn, append(juuid, 20, s.WIP2[12], s.WIP2[13], s.WIP2[14], s.WIP2[15]), raddr); e.Errlog(err) {
+		if err = s.Send(conn, append(juuid, 20, s.WIP2[12], s.WIP2[13], s.WIP2[14], s.WIP2[15]), raddr); e.Errlog(err) {
 			return
 		}
 		fmt.Println("回复了20", append(juuid, 20, s.WIP2[12], s.WIP2[13], s.WIP2[14], s.WIP2[15]))
@@ -88,10 +72,10 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 			fmt.Println("30的网关地址", raddr.String())
 
 			if strconv.Itoa(raddr.Port) == string(Port1) { //两次请求端口相同、锥形NAT，需进一步判断 回复40和50
-				if err = S(conn2, append(juuid, 40), natAddr1); e.Errlog(err) { //4
+				if err = s.Send(conn2, append(juuid, 40), natAddr1); e.Errlog(err) { //4
 					return
 				}
-				if err = S(conn, append(juuid, 50), natAddr1); e.Errlog(err) { //5
+				if err = s.Send(conn, append(juuid, 50), natAddr1); e.Errlog(err) { //5
 					return
 				}
 				s.dbd.U(string(juuid), "step", "50")
@@ -101,7 +85,7 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 				if raddr.Port == int(da[18])<<8+int(da[19]) && Port1 == s.dbd.R(string(juuid), "c1") {
 					// 两次网关端口与使用端口相同，公网IP 100
 
-					if err = S(conn, append(juuid, 100), natAddr1); e.Errlog(err) {
+					if err = s.Send(conn, append(juuid, 100), natAddr1); e.Errlog(err) {
 						return
 					}
 					s.dbd.U(string(juuid), "step", "100")
@@ -109,14 +93,14 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 				} else { //对称NAT
 
 					if raddr.Port-natAddr1.Port <= 5 { // 相连，为顺序NAT
-						if err = S(conn, append(juuid, 110), natAddr1); e.Errlog(err) {
+						if err = s.Send(conn, append(juuid, 110), natAddr1); e.Errlog(err) {
 							return
 						}
 						s.dbd.U(string(juuid), "step", "110")
 						fmt.Println("回复了110", append(juuid, 110))
 
 					} else { // 无序对称NAT
-						if err = S(conn, append(juuid, 250), natAddr1); e.Errlog(err) {
+						if err = s.Send(conn, append(juuid, 250), natAddr1); e.Errlog(err) {
 							return
 						}
 						s.dbd.U(string(juuid), "step", "250")
@@ -131,10 +115,10 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 			}
 			fmt.Println("60的网关地址", raddr.String())
 
-			if err = S(ip2conn, append(juuid, 70), raddr); e.Errlog(err) {
+			if err = s.Send(ip2conn, append(juuid, 70), raddr); e.Errlog(err) {
 				return
 			}
-			if err = S(conn, append(juuid, 80), raddr); e.Errlog(err) {
+			if err = s.Send(conn, append(juuid, 80), raddr); e.Errlog(err) {
 				return
 			}
 			s.dbd.U(string(juuid), "step", "80")
@@ -145,7 +129,7 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 			if !net.IP.Equal(raddr.IP, natAddr1.IP) { // IP 不相同 无序对称NAT
 				// IP 不相同 250
 				fmt.Println("IP 不相同；发送了250")
-				S(conn, append(juuid, 250), natAddr1)
+				s.Send(conn, append(juuid, 250), natAddr1)
 				s.dbd.U(string(juuid), "step", "250")
 
 			} else {
@@ -153,13 +137,13 @@ func (s *STUN) judgeSever(conn, conn2, ip2conn *net.UDPConn, da []byte, raddr *n
 				if (bias < 10 && bias > 0) || (bias > -10 && bias < 0) { //完全顺序对称NAT
 
 					fmt.Println("发送了230")
-					S(conn, append(juuid, 230), natAddr1)
+					s.Send(conn, append(juuid, 230), natAddr1)
 					s.dbd.U(string(juuid), "step", "230")
 
 				} else { //IP限制顺序对称NAT
 					fmt.Println("发送了240")
 
-					S(conn, append(juuid, 240), natAddr1)
+					s.Send(conn, append(juuid, 240), natAddr1)
 					s.dbd.U(string(juuid), "step", "240")
 					fmt.Println("240发送数据", append(juuid, 240))
 					fmt.Println("第一次请求地址", natAddr1.String())
@@ -243,25 +227,25 @@ func (s *STUN) judgeCliet(port int) (int, error) {
 		}
 	}
 	// 发送函数
-	var S = func(conn *net.UDPConn, da []byte, raddr *net.UDPAddr) error {
-		for i := 0; i < s.Iterate; i++ {
-			if raddr != nil {
-				if _, err := conn.WriteToUDP(da, raddr); err != nil {
-					return err
-				}
-			} else {
-				if _, err := conn.Write(da); err != nil {
-					return err
-				}
-			}
-		}
-		return nil
-	}
+	// var S = func(conn *net.UDPConn, da []byte, raddr *net.UDPAddr) error {
+	// 	for i := 0; i < s.Iterate; i++ {
+	// 		if raddr != nil {
+	// 			if _, err := conn.WriteToUDP(da, raddr); err != nil {
+	// 				return err
+	// 			}
+	// 		} else {
+	// 			if _, err := conn.Write(da); err != nil {
+	// 				return err
+	// 			}
+	// 		}
+	// 	}
+	// 	return nil
+	// }
 
 	/* 开始 */
 
 	// 发 10
-	if err = S(conn, append(da, 10, uint8(c1>>8), uint8(c1)), raddr1); e.Errlog(err) {
+	if err = s.Send(conn, append(da, 10, uint8(c1>>8), uint8(c1)), raddr1); e.Errlog(err) {
 		return -1, err
 	}
 
@@ -277,7 +261,7 @@ func (s *STUN) judgeCliet(port int) (int, error) {
 	}
 
 	// 第二端口发 30
-	if err = S(conn2, append(juuid, 30, uint8(c2>>8), uint8(c2)), nil); e.Errlog(err) {
+	if err = s.Send(conn2, append(juuid, 30, uint8(c2>>8), uint8(c2)), nil); e.Errlog(err) {
 		return -1, err
 	}
 
@@ -291,7 +275,7 @@ func (s *STUN) judgeCliet(port int) (int, error) {
 		if da[17] == 50 {
 			if da, err = R(40); err != nil {
 				if strings.Contains(err.Error(), "timeout") {
-					S(conn, append(juuid, 220), raddr1) // 收到50，收不到40; 端口限制
+					s.Send(conn, append(juuid, 220), raddr1) // 收到50，收不到40; 端口限制
 					return 220, nil
 				}
 				e.Errlog(err)
@@ -301,7 +285,7 @@ func (s *STUN) judgeCliet(port int) (int, error) {
 		// 至此，起码已经收到40；为完全锥形或IP限制锥形
 
 		// 发60
-		if err = S(conn, append(juuid, 60), raddr1); e.Errlog(err) {
+		if err = s.Send(conn, append(juuid, 60), raddr1); e.Errlog(err) {
 			return 0, err
 		}
 
@@ -313,13 +297,13 @@ func (s *STUN) judgeCliet(port int) (int, error) {
 		} else if da[17] == 80 {
 			if da, err = R(70); err != nil { //收到80，尝试接收70
 				if strings.Contains(err.Error(), "timeout") { //收不到70 IP限制锥形
-					S(conn, append(juuid, 200), raddr1)
+					s.Send(conn, append(juuid, 200), raddr1)
 					return 210, nil
 				}
 			}
 
 			// 至此，已经接收到7 完全锥形NAT
-			S(conn, append(juuid, 200), raddr1)
+			s.Send(conn, append(juuid, 200), raddr1)
 			return 200, nil
 		}
 	} else if da[17] == 90 || da[17] == 100 { // 区分具有防火墙的公网IP
@@ -327,20 +311,20 @@ func (s *STUN) judgeCliet(port int) (int, error) {
 			if da, err = R(90); err != nil {
 				if strings.Contains(err.Error(), "time") { // 收不到90
 					// 具有防火墙的公网IP
-					S(conn, append(juuid, 190), raddr1)
+					s.Send(conn, append(juuid, 190), raddr1)
 					return 190, nil
 				}
 				return -1, err
 			}
 			// 至此，至少已经收到了90 公网IP
-			S(conn, append(juuid, 180), raddr1)
+			s.Send(conn, append(juuid, 180), raddr1)
 			return 180, nil
 		}
 	} else if da[17] == 110 { // 请求sever2
 		fmt.Println("收到110")
 
 		// 第二IP
-		S(conn, append(juuid, 120), &net.UDPAddr{IP: wip2, Port: s.s1})
+		s.Send(conn, append(juuid, 120), &net.UDPAddr{IP: wip2, Port: s.s1})
 
 		// 接收回复 230 240
 		if da, err = R(230, 240, 250); err != nil {
@@ -358,4 +342,20 @@ func distinguish(err error) (int, error) {
 		return 0, errors.New("sever no reply")
 	}
 	return -1, err
+}
+
+// 回复函数
+func (s *STUN) Send(conn *net.UDPConn, da []byte, raddr *net.UDPAddr) error {
+	for i := 0; i < s.Iterate; i++ {
+		if raddr != nil {
+			if _, err := conn.WriteToUDP(da, raddr); err != nil {
+				return err
+			}
+		} else {
+			if _, err := conn.Write(da); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
