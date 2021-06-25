ff227c82ecf37c288df59cee7fcd08a07968517e
lysShub <2454148255@qq.com>
2021-06-25 23:33:10 +0800

    lysShu8b

--- internal/com/com.go
+++ internal/com/com.go
@@ -2,16 +2,21 @@ package com
 
 import (
 	"bytes"
+	"crypto/rand"
 	"io"
 	"log"
+	"math/big"
 	"net"
 	"os"
 	"runtime"
 	"strconv"
 	"strings"
 
+	"github.com/lysShub/e"
 	uuid "github.com/satori/go.uuid"
 )
+
+var err error
 
 func Errlog(err ...error) bool {
 	var haveErr bool = false
@@ -51,4 +56,54 @@ func GetLocalIP() (net.IP, error) {
 	}
 	defer con.Close()
 	return net.ParseIP(strings.Split(con.LocalAddr().String(), ":")[0]), nil
+}
+
+func RandPort() int {
+	b := new(big.Int).SetInt64(int64(52000))
+	i, err := rand.Int(rand.Reader, b)
+	r := int(i.Int64()) + 100
+	if e.Errlog(err) {
+		return 52942
+	}
+	return r
+}
+
+// 选出出现最多的值
+func selectMost(l []int) int {
+	var m map[int]int = make(map[int]int)
+	for _, v := range l {
+		m[v] = m[v] + 1
+	}
+	var c, r int = 0, 0
+	for k, v := range m {
+		if v > c {
+			c = v
+			r = k
+		}
+	}
+	return r
+}
+
+func domainToIP(sever string) (string, error) {
+	if r := net.ParseIP(sever); r == nil { //可能是域名
+		var ips []net.IP
+		if ips, err = net.LookupIP(sever); err != nil {
+			return "", err
+		}
+		for _, ip := range ips {
+			if ipv4 := ip.To4(); ipv4 != nil {
+				return ipv4.String(), nil
+			}
+		}
+	}
+	return sever, nil
+}
+
+func IsLanIP(ip net.IP) bool {
+	// (a==10 || (a==172 && (b>=16 && b<32)) || (a==192 && b==168))
+	if ip[12] == 10 || (ip[12] == 172 && (ip[13] >= 16 && ip[13] < 32)) || (ip[12] == 192 && ip[13] == 168) {
+		return true
+	} else {
+		return false
+	}
 }
--- stun.go
+++ stun.go
@@ -1,10 +1,8 @@
 package stun
 
 import (
-	"crypto/rand"
 	"errors"
 	"fmt"
-	"math/big"
 	"net"
 	"sync"
 	"time"
@@ -23,30 +21,6 @@ type STUN struct {
 	ExtPorts  int           // 泛端口范围，默认7
 
 	Port int // 端口，使用多个端口则依次递增
-
-	// /* 仅客户端配置 */
-
-	// // 服务器地址、IP或域名，仅客户端需要设置
-	// //  其对应的IP应该是服务器第一网卡的公网IP
-	// Sever string
-
-	// /* 仅服务器配置 */
-
-	// // 服务器第一网卡的局域网IP，仅服务器需要设置
-	// //  此网卡的公网IP与客户端设置的Sever应相同
-	// LIP1 net.IP
-	// // 服务器第二网卡的局域网IP，仅服务器需要设置
-	// LIP2 net.IP
-	// // 服务器第二网卡的公网网IP，仅服务器需要设置
-	// WIP2 net.IP
-
-	// /* 私有 */
-
-	// dbj *mapdb.Db // NAT类型判断的数据库
-	// dbt *mapdb.Db // NAT穿隧数据库
-	// s1  int       // 服务器第一端口，与SeverPort相同
-	// s2  int       // 服务器第二端口，与SeverPort+1相同
-	// s3  int       // 服务器第三端口，与SeverPort+2相同
 }
 
 var err error
@@ -80,11 +54,11 @@ func InitSever(port int, lip2, wip2 net.IP) (*sconn, error) {
 	s.s1 = port
 	s.s2 = port + 1
 
-	if s.lip2 = lip2; !lip2.IsGlobalUnicast() {
-		return nil, errors.New("invalid parameter lip2")
+	if s.lip2 = lip2; !com.IsLanIP(lip2) {
+		return nil, errors.New("invalid parameter 'lip2'")
 	}
-	if s.wip2 = wip2; wip2.IsGlobalUnicast() {
-		return nil, errors.New("invalid parameter wip2")
+	if s.wip2 = wip2; com.IsLanIP(wip2) {
+		return nil, errors.New("invalid parameter 'wip2'")
 	}
 	if s.lip1, err = com.GetLocalIP(); err != nil {
 		return nil, err
@@ -113,7 +87,7 @@ func (s *sconn) RunSever() error {
 	var da []byte = make([]byte, 256)
 	var raddr *net.UDPAddr
 	var n int
-	var cl sync.RWMutex
+	var cl sync.Mutex
 
 	// 第二IP接收到的数据
 	go func() {
@@ -179,11 +153,10 @@ func InitClient(port int, sever net.IP) (*cconn, error) {
 	s.c1 = port
 	s.c2 = port + 1
 
-	if s.sever = sever; sever.IsGlobalUnicast() {
-		return nil, errors.New("invalid parameter sever")
+	if s.sever = sever; com.IsLanIP(sever) {
+		return nil, errors.New("invalid parameter 'sever'")
 	}
-
-	return nil, nil
+	return s, nil
 }
 
 // RunClient
@@ -203,45 +176,4 @@ func (s *cconn) RunClient(port int, id [16]byte) error {
 	fmt.Println(raddr, rnat)
 	// return R{Raddr: raddr, RNat: rnat, LNat: lnat}, nil
 	return nil
-}
-
-func RandPort() int {
-	b := new(big.Int).SetInt64(int64(52000))
-	i, err := rand.Int(rand.Reader, b)
-	r := int(i.Int64()) + 100
-	if e.Errlog(err) {
-		return 52942
-	}
-	return r
-}
-
-// 选出出现最多的值
-func selectMost(l []int) int {
-	var m map[int]int = make(map[int]int)
-	for _, v := range l {
-		m[v] = m[v] + 1
-	}
-	var c, r int = 0, 0
-	for k, v := range m {
-		if v > c {
-			c = v
-			r = k
-		}
-	}
-	return r
-}
-
-func domainToIP(sever string) (string, error) {
-	if r := net.ParseIP(sever); r == nil { //可能是域名
-		var ips []net.IP
-		if ips, err = net.LookupIP(sever); err != nil {
-			return "", err
-		}
-		for _, ip := range ips {
-			if ipv4 := ip.To4(); ipv4 != nil {
-				return ipv4.String(), nil
-			}
-		}
-	}
-	return sever, nil
 }
